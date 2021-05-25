ecebf82b6537f23bf9e9255da23d56956fbd1f05
RuslanMirov <ruslan777mirov@gmail.com>
2021-05-25 11:02:38 +0300

    Update tests

--- test/holder-test.js
+++ test/holder-test.js
@@ -63,7 +63,7 @@ contract('Holder', function([userOne, userTwo, userThree]) {
       assert.equal(await web3.eth.getBalance(holder.address), toWei(String(10)))
     })
 
-    it('Owner can not withdarw after finish time', async function() {
+    it('Owner can withdarw after finish time', async function() {
       const ownerBalanceBefore = await web3.eth.getBalance(userOne)
       await timeMachine.advanceTimeAndBlock(duration.days(366))
       await holder.withdrawETH()
@@ -110,7 +110,7 @@ contract('Holder', function([userOne, userTwo, userThree]) {
       assert.equal(await token.balanceOf(holder.address), toWei(String(100)))
     })
 
-    it('Owner can not withdarw after finish time', async function() {
+    it('Owner can withdarw after finish time (and get all ERC20)', async function() {
       const ownerBalanceBefore = await token.balanceOf(userOne)
       await timeMachine.advanceTimeAndBlock(duration.days(366))
       await holder.withdrawERC20(token.address)
@@ -120,19 +120,18 @@ contract('Holder', function([userOne, userTwo, userThree]) {
   })
 
   describe('Emergency Withdraw ERC20', function() {
-    it('Owner can not withdarw with not correct password', async function() {
+    it('Owner can not withdraw with not correct password', async function() {
       await holder.emergencyWithdrawERC20("123", token.address).should.be.rejectedWith(EVMRevert)
       assert.equal(await token.balanceOf(holder.address), toWei(String(100)))
     })
 
-    it('Not owner can not withdarw even with correct password', async function() {
+    it('Not owner can not withdraw even with correct password', async function() {
       await holder.emergencyWithdrawERC20("12345", token.address, { from:userTwo })
       .should.be.rejectedWith(EVMRevert)
       assert.equal(await token.balanceOf(holder.address), toWei(String(100)))
     })
 
-    it('Owner can withdarw with correct password (and get all ETH)', async function() {
-      const ownerBalanceBefore = token.balanceOf(userOne)
+    it('Owner can withdraw with correct password (and get all ERC20)', async function() {
       await holder.emergencyWithdrawERC20("12345", token.address)
       assert.equal(await token.balanceOf(holder.address), 0)
       assert.equal(await token.balanceOf(userOne), toWei(String(100)))
@@ -141,6 +140,9 @@ contract('Holder', function([userOne, userTwo, userThree]) {
 
   describe('Pass permissions', function() {
     it('Not owner can not set new password', async function() {
+      await holder.emergencyWithdrawETH("12345")
+      assert.equal(await holder.isPassUsed(), true)
+
       await holder.setNewPassword(NEW_PASSWORD, { from:userTwo })
       .should.be.rejectedWith(EVMRevert)
       assert.equal(await holder.isPassUsed(), false)
@@ -226,7 +228,7 @@ contract('Holder', function([userOne, userTwo, userThree]) {
       .should.be.rejectedWith(EVMRevert)
     })
 
-    it('Owner can NOT set more than 365 days in increaseHoldTime', async function() {
+    it('Owner can NOT set more than 365 days in increaseHoldTime in one tx', async function() {
       await holder.increaseHoldTime(duration.days(366))
       .should.be.rejectedWith(EVMRevert)
     })
